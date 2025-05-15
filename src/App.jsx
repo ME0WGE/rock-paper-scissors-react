6b25e56b1e37feb319e5a15f2b28e8373d1ed422
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-15 10:11:32 +0200

    add font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas qui cum animi temporibus eius dolorem, dolore magnam minima culpa aperiam corporis fugit assumenda ullam accusamus quis, illum corrupti provident sequi.</p>
+  </>;
 }
 
 export default App;
